816922a597d0e4bf47b916fbb178c84dfb13f604
Vatsal <vatsal23.2020@outlook.com>
2026-09-19 16:51:30 +0530

    Move deployment to centralus due to capacity limits

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "location" {
   description = "The Azure region to deploy resources"
   type        = string
-  default     = "eastus2"
+  default     = "centralus"
 }
 
 variable "sql_admin_login" {
