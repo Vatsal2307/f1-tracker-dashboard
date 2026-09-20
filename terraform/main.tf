4cfa0deffb362f548a3f485ab0dea4e24263e9f4
Vatsal <vatsal23.2020@outlook.com>
2026-09-20 18:04:57 +0530

    refactor: remove Azure SQL server and database resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,35 +19,6 @@ resource "azurerm_static_web_app" "swa" {
   sku_size            = "Free"
 }
 
-# 3. SQL Server & Database (Serverless with Auto-Pause)
-resource "azurerm_mssql_server" "sql" {
-  name                         = "sql-f1-tracker-${random_integer.suffix.result}"
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
-  version                      = "12.0"
-  administrator_login          = var.sql_admin_login
-  administrator_login_password = var.sql_admin_password
-  minimum_tls_version          = "1.2"
-}
-
-# Allow Azure Services (like our Function App) to reach the SQL Server
-resource "azurerm_mssql_firewall_rule" "allow_azure" {
-  name             = "AllowAzureServices"
-  server_id        = azurerm_mssql_server.sql.id
-  start_ip_address = "0.0.0.0"
-  end_ip_address   = "0.0.0.0"
-}
-
-resource "azurerm_mssql_database" "db" {
-  name                        = "sqldb-f1-tracker"
-  server_id                   = azurerm_mssql_server.sql.id
-  collation                   = "SQL_Latin1_General_CP1_CI_AS"
-  
-  # Serverless Gen5 configuration with auto-pause to prevent billing
-  sku_name                    = "GP_S_Gen5_1"
-  min_capacity                = 0.5
-  auto_pause_delay_in_minutes = 60 
-}
 
 # 4. Function App Infrastructure (Consumption Plan)
 resource "azurerm_storage_account" "func_sa" {
